1f0ce75eb6109c7b6e7fd00f8ea40ac85193779f
Drake Wagner <drake.wagner311@gmail.com>
2020-07-11 16:39:51 -0400

    sentence word count

--- file1.py
+++ file1.py
@@ -1,1 +1,7 @@
-print('file one')
+a = 'a pig walked into a bar. He sat down and took off his hat.'
+
+def word_count(sentence):
+    words = len(sentence.split())
+    print('# of words: ', words)
+
+word_count(a)
