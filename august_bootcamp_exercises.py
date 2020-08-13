c304b888e27eb3bee0636daf24e794e5b3876b3d
DrakeWagner <dbw2tn@virginia.edu>
2020-08-12 21:49:45 -0400

    added stopwatch feature

--- august_bootcamp_exercises.py
+++ august_bootcamp_exercises.py
@@ -1,7 +1,19 @@
-from datetime import datetime
 
 import time
 
 t = time.localtime()
 current_time = time.strftime("%H:%M:%S", t)
 print(current_time)
+
+
+input('Press enter to start')
+start_time = time.time()
+print('Starting stopwatch at ', start_time)
+
+input('Press enter to stop')
+end_time = time.time()
+time_elapsed = round(end_time - start_time, 2)
+print('Time elapsed: ', time_elapsed, ' seconds.')
+
+################
+
