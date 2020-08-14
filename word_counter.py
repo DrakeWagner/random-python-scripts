a85f59e169ee8c17423f676175634013dd5d666e
DrakeWagner <dbw2tn@virginia.edu>
2020-08-13 21:57:49 -0400

    omitted characters

--- word_counter.py
+++ word_counter.py
@@ -1,16 +1,29 @@
 a = 'a pig walked into a bar. He sat down and took off his hat.'
+a_with_punct = a.split()# separates by spaces
+
+# omits = '\"!,.:;\'?'
+# for char in omits:
+#     a_omitted = a.replace(char,'')
+a_omitted = a.replace('.', '')
+a_without_punct = a_omitted.split() 
+
 
 def word_count(sentence):
-    words = len(sentence.split())
+    words = len(sentence)
     print('Number of words: ', words)
 
-word_count(a)
+print(a_without_punct)
+print(a_with_punct)
+
+word_count(a_without_punct)
+
+
 
 def check_for_word():
     word_search = input('Enter a word you would like to check for: ')
     if word_search in a:
         times = a.count(word_search)
-        print('This word occurs ', times, ' times.', sep='')
+        print('This word occurs ', times, ' time(s).', sep='')
     elif word_search not in a:
         print('This word is not in the list')
 
