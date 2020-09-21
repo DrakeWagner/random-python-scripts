f194e5482586582a141faaf1c6dd21d5c5d0d60d
DrakeWagner <dbw2tn@virginia.edu>
2020-09-20 21:47:41 -0400

    start caesar function

--- encryption.py
+++ encryption.py
@@ -1,2 +1,15 @@
 #!/bin/usr/python env
 
+# program to encrypt messages
+# start with set encryption
+# eventually add multiple characters per input to be decoded
+
+
+def caesar(text, rotation):
+    result=''
+    for i in range(len(text)):
+        char=text[i]
+
+    if char.isupper():
+        result += chr((ord(char) + rotation-65) % 26 + 65)
+    return result
