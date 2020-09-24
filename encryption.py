cff817b6167df8079a7d197619dd01a5af9f28f9
DrakeWagner <dbw2tn@virginia.edu>
2020-09-24 16:20:48 -0400

    add decryption

--- encryption.py
+++ encryption.py
@@ -11,10 +11,31 @@ letters=string.ascii_lowercase
 def encrypt_right(sentence,rotation):
     # sentence=input('Enter your message: ').lower()
     new_sentence=''
+    sentence=sentence.lower()
     for i in sentence: # ord('a') and ord('z') = 65 & 97
         num_digit = ord(i) + rotation
         new_sentence+=str((chr(num_digit)))
     print(new_sentence)
     return new_sentence
 
-encrypt_right(sentence1,5)
+def encrypt_right_interactive():
+    sentence=input('Enter your message: ').lower()
+    rotation=int(input('How many rotations: '))
+    new_sentence=''
+    for i in sentence: # ord('a') and ord('z') = 65 & 97
+        num_digit = ord(i) + rotation
+        new_sentence+=str((chr(num_digit)))
+    print(new_sentence)
+    return new_sentence
+
+def decrypt(sentence,rotation):
+    new_sentence=''
+    for i in sentence:
+        num_digit = ord(i) - rotation
+        new_sentence+=str((chr(num_digit)))
+    print(new_sentence)
+
+
+# encrypt_right(sentence1,5)
+# encrypt_right_interactive()
+decrypt('mjqqt%ymjwj', 5)
