72ec599c15f0d1432386aa2b3964877f7684d8af
DrakeWagner <dbw2tn@virginia.edu>
2020-10-03 23:04:17 -0400

    Add for arushi <3

--- encryption.py
+++ encryption.py
@@ -1,14 +1,10 @@
 #!/bin/usr/python env
-
-# program to encrypt messages
-# start with set encryption
-# eventually add multiple characters per input to be decoded
 import string
-string.ascii_lowercase[8]
-
-sentence1='Hello there'
 letters=string.ascii_lowercase
-def encrypt_right(sentence,rotation):
+
+# Special cryptography code for Drake and Arushi
+
+def encrypt(sentence,rotation):
     # sentence=input('Enter your message: ').lower()
     new_sentence=''
     sentence=sentence.lower()
@@ -18,14 +14,15 @@ def encrypt_right(sentence,rotation):
     print(new_sentence)
     return new_sentence
 
-def encrypt_right_interactive():
+def encrypt_interactive():
     sentence=input('Enter your message: ').lower()
     rotation=int(input('How many rotations: '))
     new_sentence=''
     for i in sentence: # ord('a') and ord('z') = 65 & 97
         num_digit = ord(i) + rotation
         new_sentence+=str((chr(num_digit)))
-    print(new_sentence)
+    print('Your encrypted message is: ',new_sentence, '\n\
+Your rotation code is: ', rotation, sep='')
     return new_sentence
 
 def decrypt(sentence,rotation):
@@ -35,7 +32,26 @@ def decrypt(sentence,rotation):
         new_sentence+=str((chr(num_digit)))
     print(new_sentence)
 
+def decrypt_interactive():
+    sentence=input('Enter the message to decrypt: ').lower()
+    rotation=int(input('How many rotations: '))
+    new_sentence=''
+    for i in sentence:
+        num_digit = ord(i) - rotation
+        new_sentence+=str((chr(num_digit)))
+    print('Your decrypted message is: ', new_sentence, sep='')
 
-# encrypt_right(sentence1,5)
-# encrypt_right_interactive()
-decrypt('mjqqt%ymjwj', 5)
+def interface():
+    choice=input('Encrypt or Decode <E or D>: ').lower()
+    print(choice)
+    if choice == ('e' or 'encrypt'):
+        encrypt_interactive()
+        interface()
+    elif choice == ('d' or 'decrypt'):
+        decrypt_interactive()
+        interface()
+    elif choice == ('q' or 'quit'):
+        import sys
+        sys.exit(0)
+
+interface()
