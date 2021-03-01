457d4f8639c7e7f4bb2b711c2488d4709c1f4f31
DrakeWagner <dbw2tn@virginia.edu>
2021-03-01 15:56:45 -0500

    add all teams and records

--- scrape_030121.py
+++ scrape_030121.py
@@ -13,13 +13,14 @@ soup = BeautifulSoup(src, 'html.parser') # parse/process source, to allow access
 # of certain information
 print(soup.title) # title
 
-links=soup.find_all("a")
-for i in links:   # select links with Tampa in it
-    if "Tampa" in i.text:
-        print(i)
+# links=soup.find_all("a")
+# for i in links:   # select links with Tampa in it
+#     if "Tampa" in i.text:
+#         print(i)
 
+links = []
 for i in links:
-    print(i.get('href')) # URLs
+    links.append(i.get('href')) # URLs
 
 table=soup.find_all('table')
 a = table[0]
@@ -27,23 +28,18 @@ b = a.find_all('td')[4]
 print(b)
 
 table = soup.findAll('table')[0] # select first table (Central Division)
-for tr in table.findAll('tr'): # all rows
-    for td in tr.findAll('td'): # all cells of row
-        print(td)
+# for tr in table.findAll('tr'): # all rows
+#     for td in tr.findAll('td'): # all cells of row
+#         print(td)
 
-
-
-
-
-
-
-
-
-
-# soup = BeautifulSoup(page.content, 'html.parser')
-
-# print([type(item) for item in list(soup.children)]) # element types
-# html = list(soup.children)[1]
-
-# soup.find_all('p')[0].get_text()
-# soup.findall('table table--')
+tbli = 0
+while tbli < 4:
+    table = soup.findAll('table')[tbli] # select first table (Central Division)
+    tbli+=1
+    print('')
+    for i in table.select('tr'): # iterate through table
+        col = i.select('td') 
+        if col:
+            team_name = col[0].text.strip()
+            team_rec = col[5].text.strip()
+            print('{}:  {}'.format(team_name, team_rec))
