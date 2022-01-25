6ad564c2f0acdd9a52c8d572fc7d41ff75373c27
DrakeWagner <dbw2tn@virginia.edu>
2022-01-24 19:48:12 -0500

    Create clock_face_angle.py

---
+++ random_minis/clock_face_angle.py
@@ -0,0 +1,14 @@
+# angle between the hour and minute hands of a clock
+
+def clock_angle(hour, minute):
+    hour_in_minutes = (hour * 5) + (minute / 60)
+    floating_degree_hour = hour_in_minutes * 6
+    floating_degree_minutes = minute * 6
+
+    degree_difference = floating_degree_hour - floating_degree_minutes
+
+    print(degree_difference)
+
+clock_angle(6, 30)
+clock_angle(10, 45)
+
