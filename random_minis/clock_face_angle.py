c8d62903d1ce7b9b34437076434a3d17c739d529
DrakeWagner <dbw2tn@virginia.edu>
2022-01-25 19:16:08 -0500

    Update clock_face_angle.py

--- random_minis/clock_face_angle.py
+++ random_minis/clock_face_angle.py
@@ -1,7 +1,7 @@
 # angle between the hour and minute hands of a clock
 
 def clock_angle(hour, minute):
-    hour_in_minutes = (hour * 5) + (minute / 60)
+    hour_in_minutes = (hour * 5) + ((1 / minute) * 60)
     floating_degree_hour = hour_in_minutes * 6
     floating_degree_minutes = minute * 6
 
